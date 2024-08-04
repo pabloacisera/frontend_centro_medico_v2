f5ef9510d5f8e5b0ef925f2fc01ea7a53aa437e7
pabloacisera <pabloacisera2021@gmail.com>
2024-08-04 17:50:14 -0300

    uhhh

--- src/app/page/page-administrativos/ver-turnos-admin/filter.pipe.ts
+++ src/app/page/page-administrativos/ver-turnos-admin/filter.pipe.ts
@@ -5,17 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-  transform(items: any[], searchTerm: string): any[] {
+  transform(items: any[], searchTerm: string, clientes: { [id: number]: any }): any[] {
     if (!items) return [];
     if (!searchTerm) return items;
 
     searchTerm = searchTerm.toLowerCase();
 
     return items.filter(item => {
-      return Object.keys(item).some(key => 
-        item[key].toString().toLowerCase().includes(searchTerm)
-      );
+      const clienteNombre = clientes[item.clienteId]?.nombre?.toLowerCase() || '';
+      const fecha = new Date(item.fecha).toLocaleDateString('es-ES', { day: '2-digit', month: 'long', year: 'numeric' }).toLowerCase();
+      const hora = new Date(item.fecha).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }).toLowerCase();
+
+      return clienteNombre.includes(searchTerm) ||
+             fecha.includes(searchTerm) ||
+             hora.includes(searchTerm);
     });
   }
 }
+
 
